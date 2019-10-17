0827473538c2038625b93f692fc2b8f2117a161d
anthonykao10 <anthonykao10@gmail.com>
2019-10-17 18:58:33 +0000

    fix user_params private method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,10 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
   def user_params
-    params.require(:product).permit(
+    params.require(:user).permit(
       :name,
       :email,
       :password,
