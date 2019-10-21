f07fcff8bd25a239bc1325d187ec71d0ed2b5c0c
anthonykao10 <anthonykao10@gmail.com>
2019-10-21 21:44:35 +0000

    strip whitespaces from email input, and downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 5}
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by(email: email)
+    user = User.find_by(email: email.strip.downcase)
     if user && user.authenticate(password)
       user
     else
