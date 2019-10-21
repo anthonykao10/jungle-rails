925ded758f5af4afc24fac0a29f48851a42d4c39
anthonykao10 <anthonykao10@gmail.com>
2019-10-21 21:36:11 +0000

    add authenticate_with_credentials class method to User model, and update sessions controller with it.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, presence: true
+  validates :password, presence: true, length: {minimum: 5}
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
